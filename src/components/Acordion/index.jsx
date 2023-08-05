d11ee93ff306d05e6b71d33f4c2198d36e33fb69
ZinetsVo <zinets.vo@gmail.com>
2023-08-05 10:53:39 +0300

    created acordion

---
+++ src/components/Acordion/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './styles.module.css'
+
+const Acordion = () => {
+  return (
+    <div className='acordion'></div>
+  )
+}
+
+export default Acordion
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
 import FruitList from '@/components/fruitlist'
+import Acordion from '@/components/Acordion/index'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
       <main>
         <FruitList />
+        <Acordion />
       </main>
     </>
   )
